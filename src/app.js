11fd37fcc3bc7f1ee0e609fed3e12eb4d5b25349
bcattaneo <c@ttaneo.uy>
2021-11-21 17:57:03 -0300

    bugfix: agrego módulo cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const { validarExistenciaContacto, actualizarDb, obtenerDb } = require("./utils");
 
 const claveExperiencia = "experiencia-laboral";
@@ -6,6 +7,7 @@ const claveExperiencia = "experiencia-laboral";
 let app = express();
 let db = obtenerDb();
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
